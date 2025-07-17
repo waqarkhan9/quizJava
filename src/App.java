19b69a3168bd8e720c70b516a6a3e0fe13f15379
waqar khan <waqarkhan9@yahoo.com>
2025-07-17 04:27:56 -0700

    structure of quiz done

--- src/App.java
+++ src/App.java
@@ -1,14 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class App {
 
+   
 
-
-    /* 
+    /*
      * questions array
      * score
      * result
      * 
      */
     public static void main(String[] args) throws Exception {
-        
+        int score = 0;
+        Questions qq = new Questions();
+        qq.captialQuiz();
+
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the quiz");
+        System.out.println("Type the answer to the questions");
+
+        for (Map.Entry<String, String> entry : qq.questions.entrySet()) {
+            String q = entry.getKey();
+            String a = entry.getValue();
+            System.out.println(q);
+            String res = sc.nextLine();
+
+            if (res.equalsIgnoreCase(a)) {
+                System.out.println("correct");
+                score = score +1;
+                System.out.println("Score :" + score);
+
+            } else {
+                System.out.println("Wrong");
+                System.out.println("Score :" + score);
+            }
+
+        }
+        // System.out.println("Your Score for the quiz is :" + score);
+
     }
 }
--- src/Questions.java
+++ src/Questions.java
@@ -2,14 +2,16 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.HashMap;
 
-
 public class Questions {
-    Map <String, String> questions = new HashMap<>();
-    
-    
-    
-    
+    HashMap<String, String> questions = new HashMap<String, String>();
 
-
-   
+    public void captialQuiz(){
+        questions.put("Pakistan", "Islamabad");
+        questions.put("France", "Paris");
+        questions.put("Spain", "Madrid");
+        
+    }      
+    public void mathQuiz(){
+        
+    }
 }
